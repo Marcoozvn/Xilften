75a7c325a1f5837f10350869e7cc04e7be90bcbd
Marcos Cesar <marcos.v.cesar@accenture.com>
2020-07-09 22:38:28 -0300

    Ajuste

--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:3333"
+  baseURL: 'https://xilften-backend.herokuapp.com/'
 })
 
 export const tmdb_api = axios.create({
